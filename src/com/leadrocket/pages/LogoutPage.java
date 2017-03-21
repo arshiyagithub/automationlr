4c3fda94c8be799aaf6dfcdf817248f6e36eb326
ashiraha <arshiya.bhisty@gmail.com>
2017-03-21 15:27:19 +0530

    logout page method added

--- src/com/leadrocket/pages/LogoutPage.java
+++ src/com/leadrocket/pages/LogoutPage.java
@@ -1,12 +1,20 @@
-/**
- * 
- */
 package com.leadrocket.pages;
 
-/**
- * @author ashiraha
- *
- */
-public class LogoutPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
+public class LogoutPage {
+	WebDriver driver;
+	 
+	public LogoutPage (WebDriver ldriver)
+	{
+		this.driver=ldriver;
+	}
+	@FindBy(how=How.XPATH,using="//a[@id='logout']") WebElement logout;
+	
+	public void logout(){
+		logout.click();
+	}
 }
